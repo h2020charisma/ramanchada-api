f28b43e994389671bddda315ebe7572d5def040d
vedina <jeliazkova.nina@gmail.com>
2023-08-09 19:45:22 +0300

    spectrum upload

--- app/services/upload_service.py
+++ app/services/upload_service.py
@@ -6,9 +6,11 @@ from app.models.models import Task  # Import your data models
 from pathlib import Path
 import requests
 import json 
-from  pynanomapper.datamodel.ambit import Substances
+from  pynanomapper.datamodel.ambit import Substances,SubstanceRecord,CompositionEntry,Component, Compound
 from  pynanomapper.datamodel.nexus_utils import to_nexus
+from  pynanomapper.datamodel.nexus_spectra import spe2ambit
 import nexusformat.nexus.tree as nx
+import ramanchada2 as rc2 
 
 from ..config.app_config import load_config
 
@@ -42,7 +44,7 @@ def process(file,jsonconfig,expandconfig,base_url):
         if file_extension.lower() == ".xlsx" or file_extension.lower() == ".xls":
             parse_template_wizard_files(task,base_url,file_path,jsonconfig,expandconfig)
         else: #consider a spectrum
-            parse_spectrum_files(task,base_url,file_path)
+            parse_spectrum_files(task,base_url,file_path,jsonconfig)
         
         task.completed=int(time.time() * 1000)
         
@@ -53,9 +55,46 @@ def process(file,jsonconfig,expandconfig,base_url):
     
     return task
 
-def parse_spectrum_files(task,base_url,file_path):
-    task.status="Error"
-    task.error = "not supported yet"
+def parse_spectrum_files(task,base_url,file_path,jsonconfig):
+                        #instrument,wavelength,provider,investigation,sample,sample_provider,prefix):
+    spe = rc2.spectrum.from_local_file(file_path)
+    json_data = {"instrument" : "DEFAULT", "wavelength" : "DEFAULT", "provider" : "DEFAULT",
+                 "investigation" : "DEFAULT", "sample" : "DEFAULT", "sample_provider" : "DEFAULT", 
+                 "prefix" : "NONE"}
+    if jsonconfig is None:
+        task.status="Warning"
+        task.error = "Missing jsonconfig"
+    else:    
+        json_file_path = os.path.join(UPLOAD_DIR, f"{task.id}_config.json")
+        with open(json_file_path, "wb") as f:
+            shutil.copyfileobj(jsonconfig.file, f)
+        with open(json_file_path, "r") as f:
+            json_data = json.load(f)            
+    sample=json_data["sample"]
+    papp = spe2ambit(spe.x,spe.y,spe.meta,
+                            instrument = json_data["instrument"],
+                            wavelength=json_data["wavelength"],
+                            provider=json_data["provider"],
+                            investigation=json_data["investigation"],
+                            sample=json_data["sample"],
+                            sample_provider = json_data["sample_provider"],
+                            prefix = json_data["prefix"])                       
+    substance = SubstanceRecord(name=sample,i5uuid=papp.owner.substance.uuid)
+    substance.composition = list()
+    composition_entry = CompositionEntry(component = Component(compound = Compound(name=sample),values={}))
+    substance.composition.append(composition_entry)
+    if substance.study is None:
+        substance.study = [papp]
+    else:
+        substance.study.add(papp)
+    substances = []
+    substances.append(substance)
+        #study = mx.Study(study=studies)
+    nxroot = Substances(substance=substances).to_nexus(nx.NXroot())
+    nexus_file_path = os.path.join(UPLOAD_DIR, f"{task.id}.nxs")   
+    nxroot.save(nexus_file_path,mode="w")
+    task.status="Completed"
+    task.result=f"{base_url}dataset/{task.id}.nxs",        
 
 def parse_template_wizard_files(task,base_url,file_path,jsonconfig,expandconfig=None):
     if jsonconfig is None:
