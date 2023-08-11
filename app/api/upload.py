fd4f22b293f68487fb278c57c1b5f21bebd0aa25
vedina <jeliazkova.nina@gmail.com>
2023-08-11 20:01:08 +0300

    background tasks tasks resource functional

--- app/api/upload.py
+++ app/api/upload.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, UploadFile, File, Depends
-from fastapi import Request , Query , HTTPException
+from fastapi import Request , Query , HTTPException, BackgroundTasks
 from fastapi.responses import JSONResponse, FileResponse
 from app.models.models import Task  # Import the Task model
 from app.services import upload_service
@@ -12,7 +12,7 @@ from pynanomapper.datamodel.ambit import Substances
 router = APIRouter()
 
 from ..config.app_config import load_config
-from .tasks import tasks_db
+from ..models.models import tasks_db
 
 config = load_config()
 
@@ -23,12 +23,29 @@ async def get_request(request: Request = Depends()):
     return request
 
 @router.post("/dataset")  # Use router.post instead of app.post
-async def upload_and_convert(request: Request,file: UploadFile = File(...), 
+async def upload_and_convert(request: Request,
+                             background_tasks: BackgroundTasks,
+                                file: UploadFile = File(...), 
                                 jsonconfig: UploadFile = File(None),
-                                expandconfig: UploadFile = File(None)):
+                                expandconfig: UploadFile = File(None)
+                                ):
     base_url = str(request.base_url)  
-    task =  upload_service.process(file,jsonconfig,expandconfig,base_url)
+    #background_tasks.add_task(process_long_task, data)
+    task_id = str(uuid.uuid4())
+    task = Task(
+        uri=f"{base_url}task/{task_id}",
+        id=task_id,
+        name=f"Upload file {file.filename}",
+        error=None,
+        policyError=None,
+        status="Running",
+        started=int(time.time() * 1000),
+        completed=int(time.time() * 1000),
+        result=f"{base_url}dataset/{task_id}",
+        errorCause=None
+    )      
     tasks_db[task.id] = task
+    background_tasks.add_task(upload_service.process,task,file,jsonconfig,expandconfig,base_url)
     return {"task": [task.dict()]}
 
 @router.get("/dataset/{uuid}",
