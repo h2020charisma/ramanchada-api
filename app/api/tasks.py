a0caa05d5ff18ef5ed99f55b0d5760d02b319f8b
vedina <jeliazkova.nina@gmail.com>
2023-08-11 20:48:04 +0300

    scheduler to remove tasks older than 2h

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -5,8 +5,12 @@ router = APIRouter()
 from ..models.models import tasks_db
 
 @router.get("/task/{uuid}")
-async def get_dataset(request : Request, uuid: str):
+async def get_task(request : Request, uuid: str):
     if uuid in tasks_db:
         return tasks_db[uuid]
     else:
         raise HTTPException(status_code=404, detail="Not found")
+
+@router.get("/task")
+async def get_tasks(request : Request):
+    return list(tasks_db.values())
