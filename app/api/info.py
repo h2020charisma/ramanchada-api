4daed1698100a2f597161164d0863c875d470002
vedina <jeliazkova.nina@gmail.com>
2023-08-11 11:23:56 +0300

    info router

---
+++ app/api/info.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+import subprocess
+
+router = APIRouter()
+
+@router.get("/info")
+async def get_build_number():
+    try:
+        # Run the "git rev-parse HEAD" command to get the latest commit hash
+        commit_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+        return {"build_number": commit_hash}
+    except Exception as e:
+        return {"error": str(e)}
