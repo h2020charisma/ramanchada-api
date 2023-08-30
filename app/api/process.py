1491720c1552bd24c19203bc5b9d73881593b30a
vedina <jeliazkova.nina@gmail.com>
2023-08-30 17:43:05 +0300

    processing service

--- app/api/process.py
+++ app/api/process.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from fastapi import Request , Query , HTTPException, BackgroundTasks
+from fastapi import Request , Form , HTTPException, BackgroundTasks
 from app.models.models import Task  # Import the Task model
 from app.services import  process_service
 import os
@@ -8,19 +8,39 @@ import time
 import shutil
 
 from ..models.models import tasks_db
-
+from ..services import processing_spectra as ps
 router = APIRouter()
 
-@router.post("/process")  # Use router.post instead of app.post
-async def process_hdf5(request: Request,
+
+
+config_processing = {
+    "test" : { "class" : ps.ProcessMock },
+    "calibrate" : { "class" : ps.ProcessCalibrate},
+    "peaks" : { "class" : ps.ProcessFindPeak},
+
+}
+
+def get_process_class(process_id : str):
+    if process_id in config_processing:
+        return config_processing[process_id]
+    else:
+        return None
+    
+@router.get("/process")
+async def get_tasks(request : Request):
+    return list(config_processing.keys())
+    
+@router.post("/process/{process_id}")  # Use router.post instead of app.post
+async def process_run(request: Request,
+                       process_id: str,
                        background_tasks: BackgroundTasks,
-                       dataset_uri:str = Query(..., description="dataset_uri")):
+                       dataset_uri:str = Form(...)):
     base_url = str(request.base_url)  
     task_id = str(uuid.uuid4())
     task = Task(
         uri=f"{base_url}task/{task_id}",
         id=task_id,
-        name=f"Process file {dataset_uri}",
+        name=f"Process file {dataset_uri} with {process_id}",
         error=None,
         policyError=None,
         status="Running",
@@ -30,5 +50,10 @@ async def process_hdf5(request: Request,
         errorCause=None
     )      
     tasks_db[task.id] = task
-    background_tasks.add_task(process_service.process,task,dataset_uri,base_url)
+    process_config = get_process_class(process_id);
+    if process_config is None:
+        task.status = "Error"
+        task.error = f"Invalid process_id {process_id}"    
+    else:
+        background_tasks.add_task(process_service.process,task,process_config,dataset_uri,base_url)
     return {"task": [task.dict()]}
