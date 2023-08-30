6e25a0a0bd18e10825feeae06202e227548dfb0c
vedina <jeliazkova.nina@gmail.com>
2023-08-30 17:43:05 +0300

    processing service

--- app/services/process_service.py
+++ app/services/process_service.py
@@ -1,31 +1,20 @@
-import os
-import uuid
 import time
-import shutil
 from app.models.models import Task  # Import your data models
-import h5py
-from ..config.app_config import initialize_dirs
-from pynanomapper.datamodel.nexus_parser import SpectrumParser
 
-config, UPLOAD_DIR, NEXUS_DIR, TEMPLATE_DIR = initialize_dirs()
+async def process(task : Task,process_config : dict, nexus_dataset_url: str,base_url: str):
+    try:
+        process_class = process_config["class"]
+        process_class.process(task,nexus_dataset_url,base_url)
 
-
-async def process(task : Task,nexus_dataset_url: str,base_url: str):
-    open_dataset(nexus_dataset_url,base_url)
-    task.status = "Completed"
-    task.completed=int(time.time() * 1000)
+        task.status = "Completed"
+    except (ImportError, AttributeError) as e:
+        task.status = "Error"
+        task.error = f"Failed to load plugin or class: {e}"
+    except Exception as e:
+        task.status = "Error"
+        task.error = f"{e}"   
+    task.completed=int(time.time() * 1000)  
     
 
-def open_dataset(nexus_dataset_url: str,base_url: str):
-    if nexus_dataset_url.startswith(base_url):
-        uuid = nexus_dataset_url.split("/")[-1]
-        spectrum_parser = SpectrumParser()
-        spectrum_parser.parse(os.path.join(NEXUS_DIR,f"{uuid}.nxs"))
-        # Access the spectrum data
-        for key in spectrum_parser.parsed_objects:
-            spe = spectrum_parser.parsed_objects[key]
-            print("Spectrum data", key, spe)
-            #spe.plot()     
-        
-    else:
-        pass
+
+
---
+++ app/services/processing_spectra.py
@@ -0,0 +1,39 @@
+import os
+from app.models.models import Task
+from pynanomapper.datamodel.nexus_parser import SpectrumParser
+from ..config.app_config import initialize_dirs
+
+config, UPLOAD_DIR, NEXUS_DIR, TEMPLATE_DIR = initialize_dirs()
+
+def open_dataset(nexus_dataset_url: str,base_url: str):
+    print(nexus_dataset_url,base_url)
+    if nexus_dataset_url.startswith(base_url):
+        uuid = nexus_dataset_url.split("/")[-1]
+        spectrum_parser = SpectrumParser()
+        spectrum_parser.parse(os.path.join(NEXUS_DIR,f"{uuid}.nxs"))
+        return spectrum_parser
+    else:
+        return None
+
+class ProcessMock:
+    def process(task : Task,nexus_dataset_url: str,base_url: str):
+        spectrum_parser : SpectrumParser = open_dataset(nexus_dataset_url,base_url)
+        for key in spectrum_parser.parsed_objects:
+            spe = spectrum_parser.parsed_objects[key]
+            print("Spectrum data", key, spe)
+            #spe.plot()     
+
+
+class ProcessCalibrate:
+    def process(task : Task,nexus_dataset_url: str,base_url: str):
+        pass    
+
+class ProcessFindPeak:
+    def process(task : Task,nexus_dataset_url: str,base_url: str):
+        spectrum_parser : SpectrumParser = open_dataset(nexus_dataset_url,base_url)
+        for key in spectrum_parser.parsed_objects:
+            spe = spectrum_parser.parsed_objects[key]
+            print("Spectrum data", key, spe)
+            peak_candidates = spe.find_peak_multipeak(sharpening='hht', strategy='topo')
+            fitres = spe.fit_peak_multimodel(profile='Moffat', candidates=peak_candidates, no_fit=True)
+            print(fitres.to_dataframe_peaks())
