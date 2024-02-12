09adaf221c0862e793bbfed9a893db4ccf8959fe
vedina <jeliazkova.nina@gmail.com>
2024-02-12 11:51:58 +0200

    etag

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,13 +1,21 @@
-from fastapi import APIRouter, Request, HTTPException, Response, status
+from fastapi import APIRouter, Request, HTTPException, Response, status, Header
 from fastapi.responses import JSONResponse
+from datetime import datetime
 
 router = APIRouter()
 
 from ..models.models import tasks_db
 
 @router.get("/task/{uuid}")
-async def get_task(request : Request, uuid: str, response: Response):
+async def get_task(request : Request, uuid: str, response: Response,
+            if_none_match: str = Header(None, alias="If-None-Match"),
+            if_modified_since: datetime = Header(None, alias="If-Modified-Since")
+        ):
     if uuid in tasks_db:
+        # Check Last-Modified header
+        #if if_modified_since and if_modified_since >= last_modified_time:
+        #    return JSONResponse(content=None, status_code=304)
+    
         if tasks_db[uuid].status == "Error":
             response.status_code = status.HTTP_400_BAD_REQUEST
             return tasks_db[uuid]
