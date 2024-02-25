2f40de2627251a7d31fe3b944e16e90d0b741fbf
vedina <jeliazkova.nina@gmail.com>
2024-02-25 18:18:58 +0200

    template API test

--- tests/test_api_template.py
+++ tests/test_api_template.py
@@ -4,15 +4,49 @@ from rcapi.main import app
 from pathlib import Path
 import json
 import warnings
-
-
+import pytest
+from importlib import resources
+import yaml
+import os.path 
 TEST_JSON_PATH = Path(__file__).parent / "resources/templates/dose_response.json"
 
 client = TestClient(app)
 #warnings.warn("test")
-#print("test")
 
-def test_template():
+
+@pytest.fixture(scope="module")
+def config_dict():
+    print("\nModule-level setup: Loading config or other resources")
+    with resources.path('rcapi.config', 'config.yaml') as config_path:
+        with open(config_path, 'r') as config_file:
+            CONFIG_DICT = yaml.safe_load(config_file)
+            assert "upload_dir" in CONFIG_DICT,CONFIG_DICT
+    return CONFIG_DICT
+
+@pytest.fixture
+def clean_template_dir(config_dict):
+    print("\nSetting up resources before the test")
+    TEMPLATE_DIR = os.path.join(config_dict["upload_dir"],"TEMPLATES")
+    print(TEMPLATE_DIR)
+    remove_files_in_folder(TEMPLATE_DIR)
+    # Perform setup operations here, if any
+    yield  # This is where the test runs
+    print("\nCleaning up resources after the test")
+    remove_files_in_folder(TEMPLATE_DIR)
+    # Perform cleanup operations here, if any
+
+def remove_files_in_folder(folder_path):
+    folder_path = Path(folder_path)
+    file_list = folder_path.glob('*')
+    for file_path in file_list:
+        try:
+            if file_path.is_file():
+                file_path.unlink()
+                print(f"Removed file: {file_path}")
+        except Exception as e:
+            print(f"Error removing file {file_path}: {e}")
+
+def test_template(clean_template_dir):
     response = client.get("/template")
     assert response.status_code == 200
     assert response.json() == {"template" : []}
@@ -24,7 +58,7 @@ def get_task_result(response_post):
     assert result_uuid is not None, task_json
     return result_uuid
 
-def test_upload_and_retrieve_json():
+def test_upload_and_retrieve_json(clean_template_dir):
     # Step 1: Upload JSON
     json_content = {}
     with open(TEST_JSON_PATH, "rb") as file:
