bcac5b4b0e89a6a710f7d82d302c7c133c2bdd49
Luchesar ILIEV <luchesar.iliev@gmail.com>
2024-09-13 15:37:40 +0300

    fix recursive_copy() producing empty HDF5 file

--- src/rcapi/services/convertor_service.py
+++ src/rcapi/services/convertor_service.py
@@ -126,11 +126,11 @@ def recursive_copy(src_group : h5py.Group, dst_group : h5py.Group,level=0):
     for index,key in enumerate(src_group):
         try:
             item = src_group[key]
-            if isinstance(item, h5py.Group):
+            if isinstance(item, h5pyd.Group):
                 # Create the group in the destination file
                 new_group = dst_group.create_group(key)
                 recursive_copy(item, new_group,level+1)
-            elif isinstance(item, h5py.Dataset):
+            elif isinstance(item, h5pyd.Dataset):
                 if item.shape == ():  # Scalar dataset
                     # Copy the scalar value directly
                     dst_dataset = dst_group.create_dataset(key, data=item[()])
