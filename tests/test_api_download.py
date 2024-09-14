33d5ee6901fecf5ea640eda8a45c332763e7c4c8
vedina <jeliazkova.nina@gmail.com>
2024-09-14 07:45:10 +0300

    test cleanup

--- tests/test_api_download.py
+++ tests/test_api_download.py
@@ -5,11 +5,15 @@ import h5py, h5pyd
 import tempfile
 from os import remove
 import base64
-from io import BytesIO
 import importlib.resources
 from ramanchada2.spectrum import from_local_file 
 
 client = TestClient(app)
+
+PNG_SIGNATURE  = b'\x89PNG\r\n\x1a\n'
+HDF5_SIGNATURE = b'\x89HDF\r\n\x1a\n'
+
+TEST_ENDPOINT = "/download"
 
 @pytest.fixture(scope="module")
 def domain():
@@ -19,7 +23,10 @@ def domain():
     _domain = response.json()[0]["value"]
     return _domain
 
-
+@pytest.fixture(scope="module")
+def test_spectrum():
+    return importlib.resources.files('ramanchada2.auxiliary.spectra.datasets2.FMNT-M_BW532').joinpath('nCAL10_iR532_Probe_100_2500msx3.txt')
+ 
 
 def test_access_domain(domain):
     print(h5pyd.__version__)
@@ -30,19 +37,17 @@ def test_access_domain(domain):
 
 def test_download_domain(domain):
     params = { "domain" : domain , "what" : "h5"} 
-    response = client.get("/download",params=params)
+    response = client.get(TEST_ENDPOINT,params=params)
     assert response.status_code == 200
     print(response)
 
 def test_download_domain_h5(domain):
-    print(domain)
     params = { "domain" : domain , "what" : "h5"} 
-    response = client.get("/download",params=params)
+    response = client.get(TEST_ENDPOINT,params=params)
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
     assert response.headers["Content-Type"] == "application/x-hdf5", "Response is not an HDF5 file but {}".format(response.headers["Content-Type"])
     # Check if the response content starts with HDF5 file signature
-    hdf5_signature = b'\x89HDF\r\n\x1a\n'
-    assert response.content.startswith(hdf5_signature), "Response content is not a valid HDF5 file"
+    assert response.content.startswith(HDF5_SIGNATURE), "Response content is not a valid HDF5 file"
     # Create a temporary file to store the HDF5 content
     with tempfile.NamedTemporaryFile(suffix=".h5", delete=False) as tmp_file:
         # Write the response content to the temporary file
@@ -50,10 +55,6 @@ def test_download_domain_h5(domain):
         tmp_file_path = tmp_file.name    
     # Open the saved file using h5py
     with h5py.File(tmp_file_path, 'r') as hdf:
-        # Print and inspect the HDF5 file structure (e.g., datasets, attributes)
-        print(f"Contents of the HDF5 file {tmp_file_path}:")
-        print(list(hdf.keys()))  # This will print out the datasets/groups in the file
-        # Perform assertions based on the file structure
         assert len(hdf.keys()) > 0, "HDF5 file is empty"
     # Clean up the temporary file after the test
     remove(tmp_file_path)
@@ -61,17 +62,16 @@ def test_download_domain_h5(domain):
 def test_download_domain_image(domain):
     print(domain)
     params = { "domain" : domain , "what" : "thumbnail"} 
-    response = client.get("/download",params=params)
+    response = client.get(TEST_ENDPOINT,params=params)
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
     # Assert that the Content-Type is "image/png"
     assert response.headers["Content-Type"] == "image/png", "Response is not a PNG image"
     # Check if the response content starts with PNG header bytes
-    png_signature = b'\x89PNG\r\n\x1a\n'
-    assert response.content.startswith(png_signature), "Response content is not a valid PNG image"
+    assert response.content.startswith(PNG_SIGNATURE), "Response content is not a valid PNG image"
 
 def test_download_domain_b64png(domain):
     params = { "domain" : domain , "what" : "b64png"} 
-    response = client.get("/download",params=params)
+    response = client.get(TEST_ENDPOINT,params=params)
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
     # Assert that the Content-Type is "image/png"
     assert response.headers["Content-Type"].startswith("text/plain"), "Response is not Base64-encoded PNG but {}".format(response.headers["Content-Type"])
@@ -83,24 +83,20 @@ def test_download_domain_b64png(domain):
     except Exception as e:
         assert False, f"Failed to decode Base64 data: {e}"
     # Check if the decoded data starts with the PNG signature
-    png_signature = b'\x89PNG\r\n\x1a\n'
-    assert decoded_data.startswith(png_signature), "Decoded data is not a valid PNG image"
+    assert decoded_data.startswith(PNG_SIGNATURE), "Decoded data is not a valid PNG image"
 
 def test_convert_post_files2knnquery():
     # simulate file upload
     file_path = importlib.resources.files('ramanchada2.auxiliary.spectra.datasets2.FMNT-M_BW532').joinpath('nCAL10_iR532_Probe_100_2500msx3.txt')
     with open(str(file_path), 'r') as f:
         file_content = f.read()
-    
-    # Files should be sent as a list of tuples (field_name, file_object)
+
     files = [
         ('files', (str(file_path), file_content, 'text/plain'))
     ]
-
-    # Make the POST request to the `/download` endpoint with the 'files' parameter
     response = client.post(
-        "/download", 
-        data={"what": "knnquery"},  # Send the "what" query parameter
+        TEST_ENDPOINT, 
+        params={"what": "knnquery"},  # Send the "what" query parameter
         files=files  # Send the files as multipart form-data
     )
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
@@ -108,19 +104,16 @@ def test_convert_post_files2knnquery():
     assert "cdf" in response_json
     assert "imageLink" in response_json
 
-def test_convert_post_files2b64png():
+def test_convert_post_files2b64png(test_spectrum):
     # simulate file upload
-    file_path = importlib.resources.files('ramanchada2.auxiliary.spectra.datasets2.FMNT-M_BW532').joinpath('nCAL10_iR532_Probe_100_2500msx3.txt')
-    with open(str(file_path), 'r') as f:
+    file_path = str(test_spectrum)
+    with open(str(test_spectrum), 'r') as f:
         file_content = f.read()
-    
-    # Files should be sent as a list of tuples (field_name, file_object)
     files = [
         ('files', (str(file_path), file_content, 'text/plain'))
     ]
-
     response = client.post(
-        "/download", 
+        TEST_ENDPOINT, 
         # data={"what": "b64png"},  #do we want query param or form data?
         params={"what": "b64png"}, 
         files=files  # Send the files as multipart form-data
@@ -132,12 +125,10 @@ def test_convert_post_files2b64png():
     except Exception as e:
         assert False, f"Failed to decode Base64 data: {e}"
     # Check if the decoded data starts with the PNG signature
-    png_signature = b'\x89PNG\r\n\x1a\n'
-    assert decoded_data.startswith(png_signature), "Decoded data is not a valid PNG image"
+    assert decoded_data.startswith(PNG_SIGNATURE), "Decoded data is not a valid PNG image"
 
 
-def test_access_resource():
+def test_access_resource(test_spectrum):
     # Access the resource from rc2
-    file_path = importlib.resources.files('ramanchada2.auxiliary.spectra.datasets2.FMNT-M_BW532').joinpath('nCAL10_iR532_Probe_100_2500msx3.txt')
-    from_local_file(str(file_path))
+    from_local_file(str(test_spectrum))
     
