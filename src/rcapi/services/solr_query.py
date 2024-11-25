fd1ed52bc4524ca3f539b44b463e84653921c260
vedina <jeliazkova.nina@gmail.com>
2024-11-25 10:51:33 +0200

    concise error message

--- src/rcapi/services/solr_query.py
+++ src/rcapi/services/solr_query.py
@@ -9,7 +9,7 @@ SOLR_VECTOR = config.SOLR_VECTOR
 SOLR_COLLECTION = config.SOLR_COLLECTION
 
 
-async def solr_query_post(solr_url,query_params = None,post_param = None, token = None):
+async def solr_query_post(solr_url, query_params=None, post_param=None, token=None):
     async with httpx.AsyncClient() as client:
         try:
             headers = {}
@@ -18,37 +18,37 @@ async def solr_query_post(solr_url,query_params = None,post_param = None, token 
             response = await client.post(
                 solr_url,
                 json=post_param,
-                params = query_params,
+                params=query_params,
                 headers=headers  # Pass headers
             )
             response.raise_for_status()  # Check for HTTP errors
             return response
         except httpx.HTTPStatusError as e:
-            raise HTTPException(status_code=e.response.status_code, 
-                        detail="Error fetching data from external service {}".format("(token is None)" if token is None else "(token provided)"))
+            raise HTTPException(status_code=e.response.status_code,
+                    detail="external service ({})".format("-" if token is None else "+"))
 
-async def solr_query_get(solr_url,params = None, token = None):
+
+async def solr_query_get(solr_url, params=None, token=None):
     async with httpx.AsyncClient() as client:
         try:
             headers = {}
             if token:
-                headers['Authorization'] = f'Bearer {token}'  # Add token to headers            
+                headers['Authorization'] = f'Bearer {token}'  
             response = await client.get(
                 solr_url,
-                params = params,
+                params=params,
                 headers=headers  # Pass headers
             )
             response.raise_for_status()  # Check for HTTP errors
             return response
         except httpx.HTTPStatusError as e:
             raise HTTPException(status_code=e.response.status_code, 
-                                detail="Error fetching data from external service {}".format("(token is None)" if token is None else "(token provided)"))
-        
+                    detail="external service ({})".format("-" if token is None else "+"))
+
+
 def solr_escape(value: str) -> str:
     # Escape special characters that Solr expects to be escaped
     solr_special_chars = r'(\+|\-|\&\&|\|\||!|\(|\)|\{|\}|\[|\]|\^|"|~|\*|\?|\:|\\|\/)'
-    
     # Replace the special characters with an escaped version (i.e., prefix with \)
     escaped_value = re.sub(solr_special_chars, r'\\\1', value)
-    
-    return escaped_value        
+    return escaped_value
